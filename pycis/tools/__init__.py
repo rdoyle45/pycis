7d918b67e7424724438ead53249e3b231c68dc71
jsallcock <joseph.allcock@durham.ac.uk>
2018-12-07 10:43:15 +0000

    correcting

--- pycis/tools/__init__.py
+++ pycis/tools/__init__.py
@@ -12,10 +12,7 @@ from .camera_noise_lookup import *
 from. utils import *
 from .generate_gif import *
 from .norm_height import *
-<<<<<<< HEAD
-=======
 from . img_tools import *
->>>>>>> 0a2f3ab566558de1440cfc533e819955cd5d472a
 
 try:
     from . contrast_curve import *
