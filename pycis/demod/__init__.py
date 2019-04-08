732f13fcff684a391cd03b09d63d4e509c9d29d9
Scott Silburn <s.silburn@outlook.com>
2019-04-08 21:07:49 +0100

    Add first attempt at polarisation camera demodulation (very idealised and may not even be working properly yet).

--- pycis/demod/__init__.py
+++ pycis/demod/__init__.py
@@ -3,6 +3,7 @@ from .display import *
 from .fourier_demod_2d import fourier_demod_2d
 from .fourier_demod_column import fourier_demod_column
 from .fourier_demod_1d import fourier_demod_1d
+from .polarisation_camera import polcam_demod
 from .wrap_unwrap import wrap, wrap_centre, unwrap
 from .wrap_unwrap import *
 from .mask import *
