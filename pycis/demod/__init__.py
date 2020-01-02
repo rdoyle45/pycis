50f4fba20a0f9494dd125bac00f86826e50f2758
jsallcock <joseph.allcock@durham.ac.uk>
2020-01-02 14:25:48 +0000

    init changes

--- pycis/demod/__init__.py
+++ pycis/demod/__init__.py
@@ -3,8 +3,6 @@ from .display import *
 from .fourier_demod_2d import fourier_demod_2d
 from .fourier_demod_column import fourier_demod_column
 from .fourier_demod_1d import fourier_demod_1d
-from .polarisation_camera import polcam_demod, polcam_demod2
-from .fourier_demod_doubledelay import *
 from .wrap_unwrap import wrap, unwrap
 from .wrap_unwrap import *
 from .mask import *
--- pycis/model/__init__.py
+++ pycis/model/__init__.py
@@ -3,11 +3,7 @@ from .interferometer import *
 from .bandpass_filter import *
 from .instrument import *
 from .lens import *
-# from .Lineshape import Lineshape, get_lines, create_arbitrary_lines
-
 from .dispersion import *
 from .lithium_niobate import *
-
 from .image import *
-# from .degree_coherence import degree_coherence_numerical, degree_coherence_analytical
 from .degree_coherence import *
