ef119d14db55fd8301eb5d0d91707c8d5ffc7491
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2020-03-06 15:05:53 +0000

    Add saving and binning

--- pycis/data/flow_matrix.py
+++ pycis/data/flow_matrix.py
@@ -3,14 +3,17 @@ import random
 import time
 import numpy as np
 import calcam
-from calcam.gm import misc, config
+from calcam.gm import misc, config, _bin_image, CoordTransformer, PoloidalVolumeGrid, ZipSaveFile
 import matplotlib.pyplot as plt
 import multiprocessing
 from pyEquilibrium.equilibrium import equilibrium
 from pycis.solvers import sart
 from .get import CISImage, get_Bfield
 from functools import partial
-from scipy import sparse
+from scipy import sparse, io
+import os
+import json
+
 
 class FlowGeoMatrix:
     """
@@ -65,7 +68,6 @@ class FlowGeoMatrix:
 
         self.shot = shot
         self.frame = frame
-        self.order = pixel_order
 
         b_field_funcs = get_Bfield(self.shot, self.frame)  # Functions to calculate B-field components at a given point
 
@@ -93,12 +95,41 @@ class FlowGeoMatrix:
 
         geom_data = self.geom_mat.data
 
+        if self.raydata.fullchip:
+            if self.raydata.fullchip is True:
+                raise Exception('Raydata object does not contain information on the image orientation used for ray '
+                                'casting.\n Please set the "fullchip" attribute of the raydata object to either '
+                                '"Display" or "Original".')
+            else:
+                self.image_coords = raydata.fullchip
+                self.binning = raydata.binning
+                self.pixel_order = pixel_order
+                if self.image_coords.lower() == 'original':
+                    imdims = (np.array(raydata.transform.get_original_shape()[::-1]) / self.binning).astype(int)
+                elif self.image_coords.lower() == 'display':
+                    imdims = (np.array(raydata.transform.get_display_shape()[::-1]) / self.binning).astype(int)
+                else:
+                    raise Exception("Raydata object does not contain information on the image orientation used for ray "
+                                    "casting.\n Please set the \"fullchip\" attribute of the raydata object to either "
+                                    "\"Display\" or \"Original\".")
+                self.pixel_mask = np.ones(imdims, dtype=bool)
+        else:
+
+            self.image_coords = None
+            self.binning = None
+            self.pixel_order = None
+            self.pixel_mask = None
+
+        self.image_geometry = raydata.transform
+        self.history = {'los': raydata.history, 'grid': grid.history, 'matrix': 'Created by {:s} on {:s} at {:s}'.format(misc.username, misc.hostname,
+                        misc.get_formatted_time())}
+
         # Solve y = Ax + b for x, the inverted emissivity matrix
         if inv_emis is None:
             self.inv_emis = sart.solve(geom_data, self._data_vector())[0]
 
-        ray_start_coords = self.raydata.ray_start_coords.reshape(-1, 3, order=self.order)
-        ray_end_coords = self.raydata.ray_end_coords.reshape(-1, 3, order=self.order)
+        ray_start_coords = self.raydata.ray_start_coords.reshape(-1, 3, order=self.pixel_order)
+        ray_end_coords = self.raydata.ray_end_coords.reshape(-1, 3, order=self.pixel_order)
 
         # Number of grid cells and sight lines
         n_cells = self.grid.n_cells
@@ -147,6 +178,334 @@ class FlowGeoMatrix:
                                               shape=(n_los, n_cells))
 
         self.data = weighting_matrix.multiply(b_l_sparse_matrix)
+
+    def set_binning(self, binning):
+        """
+        Set the level of image binning. Can be used to
+        decrease the size of the matrix to reduce memory or
+        computation requirements for inversions.
+
+        Parameters:
+
+            binning (float) : Desired image binning. Must be larger than the \
+                              existing binning value.
+
+        """
+        if binning < self.binning:
+            raise ValueError('Specified binning is lower than existing binning! The binning can only be increased.')
+        elif binning == self.binning:
+            return
+        else:
+
+            if self.image_coords is not None:
+                if self.image_coords.lower() == 'display':
+                    image_dims = self.image_geometry.get_display_shape()
+                elif self.image_coords.lower() == 'original':
+                    image_dims = self.image_geometry.get_original_shape()
+                else:
+                    raise Exception('Raydata object does not contain information on the image orientation used for ray '
+                                    'casting.\n Please set the "fullchip" attribute of the raydata object to either '
+                                    '"Display" or "Original".')
+            else:
+                raise Exception('Nope, no worky.')
+
+            bin_factor = int(binning / self.binning)
+
+            init_shape = (np.array(image_dims) / self.binning).astype(np.uint32)
+            row_inds = np.arange(np.prod(init_shape), dtype=np.uint32)
+            row_inds = np.reshape(row_inds, init_shape, order=self.pixel_order)
+
+            px_mask = self.pixel_mask.reshape(self.pixel_mask.size, order=self.pixel_order)
+            index_map = np.zeros(np.prod(image_dims) // int(self.binning ** 2), dtype=np.uint32)
+            index_map[px_mask == True] = np.arange(np.count_nonzero(px_mask), dtype=np.uint32)
+
+            self.pixel_mask = _bin_image(self.pixel_mask, bin_factor, bin_func=np.min).astype(bool)
+
+            px_mask = self.pixel_mask.reshape(self.pixel_mask.size, order=self.pixel_order)
+
+            ind_arrays = []
+            for colshift in range(bin_factor):
+                for rowshift in range(bin_factor):
+                    ind_arrays.append(row_inds[colshift::bin_factor, rowshift::bin_factor].reshape(
+                        int(np.prod(init_shape) / bin_factor ** 2), order=self.pixel_order))
+                    ind_arrays[-1] = index_map[ind_arrays[-1]][px_mask == True]
+
+            new_data = self.data[ind_arrays[0], :]
+            for indarr in ind_arrays[1:]:
+                new_data = new_data + self.data[indarr, :]
+
+            norm_factor = sparse.diags(np.ones(self.grid.n_cells) / bin_factor ** 2, format='csr')
+
+            self.data = new_data * norm_factor
+
+            self.binning = binning
+
+    def set_included_pixels(self, pixel_mask, coords=None):
+        """
+        Set which image pixels should be included, or not. Can be
+        used to exclude image pixels which are known to have bad data or
+        otherwise do not conform to the assumptions of the inversion.
+
+        Note: excluding pixels is a non-reversible process since their
+        matrix rows will be removed. It is therefore recommended to keep a
+        master copy of the matrix with all pixels included and then
+        use this function on a transient copy of the matrix.
+
+        Parameters:
+
+            pixel_mask (numpy.ndarray) : Boolean array the same shape as the un-binned \
+                                         camera image, where True or 1 indicates a \
+                                         pixel to be included and False or 0 represents \
+                                         a pixel to be excluded.
+
+            coords (str)               : Either 'Display' or 'Original', \
+                                         specifies what orientation the input \
+                                         pixel mask is in. If not givwn, it will be \
+                                         auto-detected if possible.
+        """
+        if self.pixel_mask is None:
+            raise Exception('Cannot set pixel mask for a geometry matrix which does not include full sensor.')
+
+        if coords is None:
+
+            # If there are no transform actions, we have no problem.
+            if len(self.image_geometry.transform_actions) == 0 and self.image_geometry.pixel_aspectratio == 1:
+                coords = self.image_coords
+
+            elif self.image_geometry.get_display_shape() != self.image_geometry.get_original_shape():
+
+                if pixel_mask.shape == self.image_geometry.get_display_shape()[::-1]:
+                    coords = 'Display'
+                elif pixel_mask.shape == self.image_geometry.get_original_shape()[::-1]:
+                    coords = 'Original'
+                else:
+                    raise ValueError(
+                        'Input pixel mask has an unexpected shape! Got {:d}x{:d}; expected {:d}x{:d} or {:d}x{:d}'.format(
+                            image.shape[1], image.shape[0], self.image_geometry.get_display_shape[1],
+                            self.image_geometry.get_display_shape[0], self.image_geometry.get_original_shape[1],
+                            self.image_geometry.get_original_shape[0]))
+            else:
+                raise Exception(
+                    'Cannot determine mask orientation automatically; please provide the "coords" input argument.')
+
+        if coords.lower() == 'display' and self.image_coords.lower() == 'original':
+            pixel_mask = self.image_geometry.display_to_original_image(pixel_mask.astype(int), interpolation='nearest')
+
+        elif coords.lower() == 'original' and self.image_coords.lower() == 'display':
+            pixel_mask = self.image_geometry.original_to_display_image(pixel_mask.astype(int), interpolation='nearest')
+
+        if self.binning > 1:
+            pixel_mask = _bin_image(pixel_mask, self.binning, bin_func=np.min)
+
+        mask_delta = self.pixel_mask.astype(int) - pixel_mask.astype(int)
+
+        if mask_delta.min() == -1:
+            raise ValueError('Provided pixel mask includes previously excluded pixels; pixels can only be '
+                             'disabled, not re-enabled using this function.')
+
+        old_px_mask = self.pixel_mask.reshape(self.pixel_mask.size, order=self.pixel_order)
+        mask_delta = mask_delta.reshape(mask_delta.size, order=self.pixel_order)
+
+        mask_delta = mask_delta[old_px_mask == True]
+
+        self.data = self.data[mask_delta == 0, :]
+
+        self.pixel_mask = pixel_mask.astype(bool)
+
+    def get_included_pixels(self):
+        """
+        Get a mask showing which image pixels are included in the
+        geometry matrix.
+
+        Returns:
+
+            numpy.ndarray : Boolean array the same shape as the camera image after binning, \
+                            where True indicates the corresponding pixel is included \
+                            and False indicates the pixel is excluded.
+        """
+        return self.pixel_mask
+
+    def save(self, filename):
+        """
+        Save the geometry matrix to a file.
+
+        Parameters:
+
+            filename (str) : File name to save to, including file extension. \
+                             The file extension determines the format to be saved: \
+                             '.npz' for compressed NumPy binary format or \
+                             '.mat' for MATLAB format
+
+        Note: .npz is the recommended format; .mat is provided if compatibility
+        with MATLAB is required but produces larger file sizes.
+        """
+        try:
+            fmt = filename.split('.')[1:][-1]
+        except IndexError:
+            raise ValueError(
+                'Given file name does not include file extension; extension .npz, .mat or .zip must be '
+                'included to determine file type!')
+
+        if fmt == 'npz':
+            self._save_npz(filename)
+        elif fmt == 'mat':
+            self._save_matlab(filename)
+        else:
+            raise ValueError('File extension "{:s}" not understood; options are "npz", "mat" or "zip".'.format(fmt))
+
+    def format_image(self, image, coords=None):
+        """
+        Format a given 2D camera image in to a 1D data vector
+        (i.e. :math:`b` in :math:`Ax = b`) appropriate for use with this
+        geometry matrix. This will bin the image, remove any excluded
+        pixels and reshape it to a 1D vector.
+
+        Parameters:
+
+            image (numpy.ndarray) : Input image.
+
+            coords (str)          : Either 'Display' or 'Original', \
+                                    specifies what orientation the input \
+                                    image is in. If not givwn, it will be \
+                                    auto-detected if possible.
+
+        Returns:
+
+            scipy.sparse.csr_matrix : 1xN_pixels image data vector. Note that this is \
+                                      returned as a sparse matrix object despite its \
+                                      density being 100%; this is for consistency with the \
+                                      matrix itself.
+
+        """
+        if coords is None:
+
+            # If there are no transform actions, we have no problem.
+            if len(self.image_geometry.transform_actions) == 0 and self.image_geometry.pixel_aspectratio == 1:
+                coords = self.image_coords
+
+            elif self.image_geometry.get_display_shape() != self.image_geometry.get_original_shape():
+
+                if image.shape == self.image_geometry.get_display_shape()[::-1]:
+                    coords = 'Display'
+                elif image.shape == self.image_geometry.get_original_shape()[::-1]:
+                    coords = 'Original'
+                else:
+                    raise ValueError(
+                        'Input image has an unexpected shape! Got {:d}x{:d}; expected {:d}x{:d} or {:d}x{:d}'.format(
+                            image.shape[1], image.shape[0], self.image_geometry.get_display_shape[1],
+                            self.image_geometry.get_display_shape[0], self.image_geometry.get_original_shape[1],
+                            self.image_geometry.get_original_shape[0]))
+            else:
+                raise Exception(
+                    'Cannot determine image orientation automatically; please provide the "coords" input argument.')
+
+        if coords.lower() == 'display' and self.image_coords.lower() == 'original':
+            im_out = self.image_geometry.display_to_original_image(image)
+
+        elif coords.lower() == 'original' and self.image_coords.lower() == 'display':
+            im_out = self.image_geometry.original_to_display_image(image)
+
+        else:
+            im_out = image.copy()
+
+        if self.binning > 1:
+            im_out = _bin_image(im_out, self.binning, bin_func=np.mean)
+
+        elif self.binning < 1:
+            raise Exception('This matrix has binning < 1 which is not really meaningful. '
+                            'Set binning =>1 before trying to use this matrix.')
+
+        im_out = im_out.reshape(im_out.size, order=self.pixel_order)
+
+        return sparse.csr_matrix(im_out[self.pixel_mask.reshape(self.pixel_mask.size, order=self.pixel_order) == True])
+
+    def _save_npz(self, filename):
+        """
+        Save the geometry matrix in compressed NumPy binary format.
+        """
+        coo_data = self.data.tocoo()
+
+        np.savez_compressed(filename,
+                            mat_row_inds=coo_data.row,
+                            mat_col_inds=coo_data.col,
+                            mat_data=coo_data.data,
+                            mat_shape=self.data.shape,
+                            grid_verts=self.grid.vertices,
+                            grid_cells=self.grid.cells,
+                            grid_wall=self.grid.wall_contour,
+                            binning=self.binning,
+                            pixel_order=self.pixel_order,
+                            pixel_mask=self.pixel_mask,
+                            history=self.history,
+                            grid_type=self.grid.__class__.__name__,
+                            im_transforms=self.image_geometry.transform_actions,
+                            im_px_aspect=self.image_geometry.pixel_aspectratio,
+                            im_shape=self.pixel_mask.shape[::-1],
+                            im_coords=self.image_coords
+                            )
+
+    def _load_npz(self, filename):
+        """
+        Load a geometry matrix from a compressed NumPy binary file
+        """
+        f = np.load(filename)
+
+        self.binning = float(f['binning'])
+        self.pixel_order = str(f['pixel_order'])
+        self.history = f['history'].item()
+        self.pixel_mask = f['pixel_mask']
+        self.image_coords = str(f['im_coords'])
+        self.image_geometry = CoordTransformer()
+        self.image_geometry.set_transform_actions(f['im_transforms'])
+        self.image_geometry.set_pixel_aspect(f['im_px_aspect'], relative_to='Original')
+        self.image_geometry.set_image_shape(*self.binning * np.array(self.pixel_mask.shape[::-1]),
+                                            coords=self.image_coords)
+
+        self.grid = PoloidalVolumeGrid(f['grid_verts'], f['grid_cells'], f['grid_wall'], src=self.history['grid'])
+        self.data = sparse.csr_matrix((f['mat_data'], (f['mat_row_inds'], f['mat_col_inds'])), shape=f['mat_shape'])
+
+    def _save_matlab(self, filename):
+        """
+        Save the geometry matrix in MATLAB format.
+        """
+        io.savemat(filename,
+                         {'geom_mat': self.data,
+                          'grid_verts': self.grid.vertices,
+                          'grid_cells': self.grid.cells,
+                          'grid_wall': self.grid.wall_contour,
+                          'binning': self.binning,
+                          'pixel_order': self.pixel_order,
+                          'sightline_history': self.history['los'],
+                          'matrix_history': self.history['matrix'],
+                          'grid_history': self.history['grid'],
+                          'pixel_mask': self.pixel_mask,
+                          'grid_type': self.grid.__class__.__name__,
+                          'im_transforms': np.array(self.image_geometry.transform_actions, dtype=np.object),
+                          'im_px_aspect': self.image_geometry.pixel_aspectratio,
+                          'im_coords': self.image_coords
+                          }
+                         )
+
+    def _load_matlab(self, filename):
+        """
+        Load geometry matrix from a MATLAB file.
+        """
+        f = io.loadmat(filename)
+
+        self.binning = float(f['binning'][0, 0])
+        self.pixel_order = str(f['pixel_order'][0])
+        self.pixel_mask = f['pixel_mask']
+        self.history = {'los': str(f['sightline_history'][0]), 'grid': str(f['grid_history'][0]),
+                        'matrix': str(f['matrix_history'][0])}
+        self.image_coords = str(f['im_coords'][0])
+
+        self.grid = PoloidalVolumeGrid(f['grid_verts'], f['grid_cells'], f['grid_wall'], src=self.history['grid'])
+        self.data = f['geom_mat'].tocsr()
+
+        self.image_geometry = CoordTransformer()
+        self.image_geometry.set_transform_actions(f['im_transforms'])
+        self.image_geometry.set_pixel_aspect(f['im_px_aspect'], relative_to='Original')
+        self.image_geometry.set_image_shape(*self.binning * np.array(self.pixel_mask.shape[::-1]), coords=self.image_coords)
 
     def _data_vector(self):
 
@@ -339,6 +698,6 @@ def _weighting_matrix(data, n_los, inv_emis):
     weight_colinds = np.asarray(weight_colinds).reshape(-1, )
     weight_values = np.asarray(weight_values).reshape(-1, )
 
-    return  weight_rowinds, weight_colinds, weight_values
+    return weight_rowinds, weight_colinds, weight_values
 
 
