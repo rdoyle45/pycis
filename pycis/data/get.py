4e48b35bda95f1e619ce3eb42f8f69808d0880c1
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2021-09-30 14:28:18 +0100

    switch to 29541

--- pycis/data/get.py
+++ pycis/data/get.py
@@ -65,7 +65,8 @@ class CISImage():
  #               from PIL import Image
   #              im = Image.open('/pfs/work/g2rdoyl/CIS/Wavelength_test/' + str(f) + '.tif')
 #                self.raw_data = np.array(im) 
-                self.raw_data = np.load('/pfs/work/g2rdoyl/CIS/25028/from_scott/Pre_pycis_synthetic/Clean/image_clean.npy')
+                #self.raw_data = np.load('/pfs/work/g2rdoyl/CIS/25028/from_scott/Pre_pycis_synthetic/Clean/image_clean.npy')
+                self.raw_data = np.load('/pfs/work/g2rdoyl/CIS/29541/29541_data_new.npz')
                 self.time = 0.31
 
         # Get calibrations
