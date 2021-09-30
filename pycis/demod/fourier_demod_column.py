d6d09891f505ae710080388f568b6a08be4e403d
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2021-09-30 15:39:38 +0100

    revert to old phi calc

--- pycis/demod/fourier_demod_column.py
+++ pycis/demod/fourier_demod_column.py
@@ -118,18 +118,22 @@ def fourier_demod_column(max_grad, window_width, Ilim, wtype, wfactor, filtval, 
 
         col_in *= scipy.signal.windows.tukey(col_in.shape[0], alpha=0.1)
    #     S_apodised = grad
-    #fft_carrier = np.fft.rfft(col_in)
-    #fft_carrier = np.multiply(fft_carrier,2*window)
-    #carrier = np.fft.irfft(fft_carrier, n=col_length)
+    fft_carrier = np.fft.rfft(col_in)
+
+    fft_length = fft_carrier.size
+    window = pycis.demod.window(fft_length, nfringes, width_factor=wfactor, fn=wtype)
+
+    fft_carrier = np.multiply(fft_carrier,2*window)
+    carrier = np.fft.irfft(fft_carrier, n=col_length)
 
     ###### TEST SCOTTS CODE #####
-    fft_carrier = np.fft.fft(col_in)
+  #  fft_carrier = np.fft.fft(col_in)
 
-    wdw_carrier = np.zeros((col_length))
-    wdw_carrier[nfringes-halfwidth:nfringes+halfwidth] = 2*scipy.signal.windows.tukey(N)
+   # wdw_carrier = np.zeros((col_length))
+   # wdw_carrier[nfringes-halfwidth:nfringes+halfwidth] = 2*scipy.signal.windows.tukey(N)
 
-    fft_carrier = fft_carrier*wdw_carrier.T
-    carrier = np.fft.ifft(fft_carrier).real.T
+   # fft_carrier = fft_carrier*wdw_carrier.T
+   # carrier = np.fft.ifft(fft_carrier).real.T
 
     analytic_signal = scipy.signal.hilbert(carrier)
     phase = np.angle(analytic_signal)
