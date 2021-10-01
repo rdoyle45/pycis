d67a9e28660071fd7bbd7a6918559509f8cf8664
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2021-10-01 11:12:54 +0100

    test128

--- pycis/data/get.py
+++ pycis/data/get.py
@@ -65,7 +65,7 @@ class CISImage():
  #               from PIL import Image
   #              im = Image.open('/pfs/work/g2rdoyl/CIS/Wavelength_test/' + str(f) + '.tif')
 #                self.raw_data = np.array(im) 
-                self.raw_data = np.load('/pfs/work/g2rdoyl/CIS/25028/from_scott/Pre_pycis_synthetic/1024/image_maxsig_1024.npy')
+                self.raw_data = np.load('/pfs/work/g2rdoyl/CIS/25028/from_scott/Pre_pycis_synthetic/128/image_maxsig_128.npy')
                 #self.raw_data = np.load('/pfs/work/g2rdoyl/CIS/29541/208/29541_data_new.npz')['raw']
                 self.time = 0.313
 
