67dcb7dae9fa52ecec58569d0a978e6e4b2f8c4c
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2022-01-25 11:09:52 +0000

    debug

--- pycis/demod/fourier_demod_column.py
+++ pycis/demod/fourier_demod_column.py
@@ -101,6 +101,7 @@ def fourier_demod_column(max_grad, window_width, Ilim, wtype, wfactor, filtval, 
     fft_dc = fft_col*wdw.T
 
     dc = 2*np.fft.ifft(fft_dc)
+    print(dc)
     dc = scipy.ndimage.filters.median_filter(dc, w)
     dc_smooth = dc
 
