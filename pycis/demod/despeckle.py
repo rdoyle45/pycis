bb08f6d324d40400dc0b27db4d7f1a6a68993a81
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2022-01-25 14:10:18 +0000

    add back row filter

--- pycis/demod/despeckle.py
+++ pycis/demod/despeckle.py
@@ -29,46 +29,46 @@ def despeckle(image, dval, display=False):
     image_out = scipy.signal.medfilt(image, dval)
 
     # loop over image rows
-    # y_dim, x_dim = image_out.shape
-    # filtered_pixels = np.zeros_like(image, dtype=bool)
-    #
-    # for row in range(y_dim):
-    #
-    #     data_row = image_out[row, :]
-    #
-    #     # tuning parameters as in Scott's original code
-    #     intensity_lim = 5  # Ilim - don't bother with parts of the image below this many counts
-    #     detection_lim = 0.05  # Detection limit for speckles, lower = more sensitive.
-    #     size_lim = 3  # Size limit - maximum number of pixels across to be considered a speckle.
-    #
-    #     # Intensity gradient down column
-    #     grad = np.gradient(data_row)
-    #     grad[data_row < intensity_lim] = 0
-    #     grad = grad / np.std(grad)
-    #
-    #     # define the normalised detection threshold fed into peakutils.peak.indexes
-    #     thres_normalised = (detection_lim - min(grad)) / (max(grad) - min(grad))
-    #     starts = indexes(grad, thres=thres_normalised)
-    #     ends = indexes(-1 * grad, thres=thres_normalised)
-    #
-    #     if np.size(starts) == 0:
-    #         if display:
-    #             print('-- despeckle: no large speckles found')
-    #     else:
-    #         # If detected speckle is below size limit, replace with the mean of its neighbouring pixels
-    #         for i in range(np.size(starts)):
-    #             endsind = find_nearest(ends, starts[i])
-    #             if (np.size(endsind) != 0 and ends[endsind] <= (starts[i] + size_lim)) and (starts[i] > 2) and ((ends[endsind] + 2) < x_dim):
-    #
-    #                 x = np.arange(starts[i], ends[endsind] + 1)  # The x-coordinates of the interpolated values
-    #                 xp = [starts[i] - 2, ends[endsind] + 2]  # The x-coordinates of the synth_data points, must be increasing
-    #                 fp = data_row[xp]  # The y-coordinates of the synth_data points, same length as xp
-    #                 data_row[x] = np.interp(x, xp, fp)
-    #
-    #                 filtered_pixels[row, starts[i]:ends[endsind]] = 1
-    #                 image_out[row, :] = data_row
-    #
-    #             elif display: print('-- despeckle: no speckles found in row')
+    y_dim, x_dim = image_out.shape
+    filtered_pixels = np.zeros_like(image, dtype=bool)
+
+    for row in range(y_dim):
+
+        data_row = image_out[row, :]
+
+        # tuning parameters as in Scott's original code
+        intensity_lim = 5  # Ilim - don't bother with parts of the image below this many counts
+        detection_lim = 0.05  # Detection limit for speckles, lower = more sensitive.
+        size_lim = 3  # Size limit - maximum number of pixels across to be considered a speckle.
+
+        # Intensity gradient down column
+        grad = np.gradient(data_row)
+        grad[data_row < intensity_lim] = 0
+        grad = grad / np.std(grad)
+
+        # define the normalised detection threshold fed into peakutils.peak.indexes
+        thres_normalised = (detection_lim - min(grad)) / (max(grad) - min(grad))
+        starts = indexes(grad, thres=thres_normalised)
+        ends = indexes(-1 * grad, thres=thres_normalised)
+
+        if np.size(starts) == 0:
+            if display:
+                print('-- despeckle: no large speckles found')
+        else:
+            # If detected speckle is below size limit, replace with the mean of its neighbouring pixels
+            for i in range(np.size(starts)):
+                endsind = find_nearest(ends, starts[i])
+                if (np.size(endsind) != 0 and ends[endsind] <= (starts[i] + size_lim)) and (starts[i] > 2) and ((ends[endsind] + 2) < x_dim):
+
+                    x = np.arange(starts[i], ends[endsind] + 1)  # The x-coordinates of the interpolated values
+                    xp = [starts[i] - 2, ends[endsind] + 2]  # The x-coordinates of the synth_data points, must be increasing
+                    fp = data_row[xp]  # The y-coordinates of the synth_data points, same length as xp
+                    data_row[x] = np.interp(x, xp, fp)
+
+                    filtered_pixels[row, starts[i]:ends[endsind]] = 1
+                    image_out[row, :] = data_row
+
+                elif display: print('-- despeckle: no speckles found in row')
     #
     #
     # if display:
